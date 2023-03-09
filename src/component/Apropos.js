2b3c460c8eb45e1af2a6705f010479fcb6bbb71a
elieKA96 <mbuyieliek@gmail.com>
2023-03-09 13:02:15 +0100

    new add

--- src/component/Apropos.js
+++ src/component/Apropos.js
@@ -7,14 +7,14 @@ function Apropos() {
         <div className={` containerMd ${style.content}`}>
             <h1 className='title mb-10'>Apropos</h1>
             <div >
-                <span><i class="fa-solid fa-angle-left"></i></span>*
+
                 <p>Récemment diplômé de bachelier en Informatique
                     de L’université technique de communication et de
                     l’informatique de Moscou, passionné de nouvelle
                     technologie et de la programmation web et aussi ouvert
                     à m’améliorer dans certains domaine comme dans l’analyse
                     des données.</p>
-                <span><i class="fa-solid fa-angle-right"></i></span>
+
             </div>
 
         </div>
